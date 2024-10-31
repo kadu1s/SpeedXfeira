d7c748f550dc21e492f2e97e2d28d8678c2e25d7
kadu1s <165293077+kadu1s@users.noreply.github.com>
2024-10-30 22:35:37 -0300

    Initial commit

---
+++ pizzas.js
@@ -0,0 +1,13 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Ticket',
+        img: 'imagens/ticket.png',
+        price: [1.00, 1.00, 1.00],
+        sizes: [
+          
+        ],
+        description: ''
+      },
+     
+];
